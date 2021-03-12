147d22098d38c94e91baae70d63e1cb05821d2cc
kmswlee <wodndl895@kookmin.ac.kr>
2021-03-12 17:56:39 +0900

    문자열

---
+++ BOJ/1120.py
@@ -0,0 +1,9 @@
+A,B = input().split()
+res = 51
+for i in range(len(B)-len(A)+1):
+    cnt = 0
+    for j in range(len(A)):
+        if A[j] != B[i+j]:
+            cnt += 1
+    res = min(res,cnt)
+print(res)

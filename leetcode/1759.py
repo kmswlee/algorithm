da3f98c17a4531a7787ca0d04c12d15e7e68d7f8
kmswlee <wodndl895@kookmin.ac.kr>
2021-04-13 20:46:32 +0900

    CountHomogenous

---
+++ leetcode/1759.py
@@ -0,0 +1,7 @@
+from itertools import groupby
+def countHomogenous(self, s: str) -> int:
+    res = 0
+    for c, s in groupby(s):
+        n = len(list(s))
+        res += n * (n + 1) // 2
+    return res % (10**9 + 7)

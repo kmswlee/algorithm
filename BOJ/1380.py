fc53601dd4ed66616194a9834fa016974b9c71a8
kmswlee <wodndl895@kookmin.ac.kr>
2021-05-02 19:47:33 +0900

    귀걸이

---
+++ BOJ/1380.py
@@ -0,0 +1,19 @@
+student=[]
+student_print=[]
+while True:
+    n=int(input())
+    if n == 0:
+        break
+    for _ in range(n):
+        student.append(input())
+    ans=0
+    for _ in range(n*2-1):
+        num,alpha = map(str,input().split())
+        ans^=int(num)
+    student_print.append(student[ans-1])
+    student.clear()
+
+for i,v in enumerate(student_print):
+    print(i+1,v)
+
+
